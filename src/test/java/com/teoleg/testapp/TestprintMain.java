6326f7b3a27963fe68a0a8d7dfc1382acddd84ea
teoleg <oleg_terletsky@hotmail.com>
2017-07-09 17:34:00 -0400

    Add main test

---
+++ src/test/java/com/teoleg/testapp/TestprintMain.java
@@ -0,0 +1,18 @@
+package com.teoleg.testapp;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class TestprintMain {
+
+	@Test
+	public void testPrintHelloWorld() {
+		
+		testapp App = new testapp();
+
+		//Assert.assertEquals(App.main(), "");
+
+	}
+
+}
+
